676a786e2409320b25397413b4f9b9043dc29306
MSmirnov <kroligne@gmail.com>
2021-11-17 11:03:00 +0300

    login and logout

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
 
-# Create your views here.
+from authapp.forms import ShopUserLoginForm
+
+
+def login(request):
+    login_form = ShopUserLoginForm(data=request.POST)
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+    context = {
+        'login_form': login_form
+    }
+    return render(request, 'authapp/login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+def edit(request):
+    return HttpResponseRedirect(reverse('index'))
+
+
+def register(request):
+    return HttpResponseRedirect(reverse('index'))
