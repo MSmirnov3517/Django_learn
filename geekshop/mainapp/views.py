b1e506c5fae2bc2b3fe3fb3ddffb08da196ac1b6
MSmirnov <kroligne@gmail.com>
2021-11-18 13:00:47 +0300

    hot_product same_product

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,8 @@
+import random
+
 from django.shortcuts import render
 from mainapp.models import Product, ProductCategory
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -18,10 +21,31 @@ def contact(request):
 
 
 def products(request, pk=None):
+    links_menu = ProductCategory.objects.all()
+    if pk is not None:
+        if pk == 0:
+            products_list = Product.objects.all()
+            category_item = {
+                'name': 'все',
+                'pk': 0
+            }
+        else:
+            category_item = get_object_or_404(ProductCategory, pk=pk)
+            products_list = Product.objects.filter(category__pk=pk)
+        context = {
+            'links_menu': links_menu,
+            'title': 'Продукты',
+            'category': category_item,
+            'products': products_list
+        }
+        return render(request, 'mainapp/products_list.html', context=context)
+
+    hot_product = random.sample(list(Product.objects.all()), 1)[0]
+    same_products = Product.objects.all()[3:5]
     context = {
-        'links_menu' : ProductCategory.objects.all(),
+        'links_menu': links_menu,
         'title': 'Продукты',
+        'hot_product': hot_product,
+        'same_products': same_products
     }
     return render(request, 'mainapp/products.html', context=context)
-
-
